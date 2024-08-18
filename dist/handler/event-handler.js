d7a7dbcdcefeac2b23536fed88765ddb0a8790dd
shamxl <shamxl@pm.me>
2024-08-18 19:28:40 +0530

    oops

--- dist/handler/event-handler.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.eventEmitter = void 0;
-const events_1 = require("events");
-exports.eventEmitter = new events_1.EventEmitter();
